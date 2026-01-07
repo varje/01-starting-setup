a1402b610f0ebd4e113870675447f12b4cf8ab44
varje <varjekivila@gmail.com>
2026-01-07 14:54:54 +0200

    rename expenses list to items

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,28 +1,28 @@
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
-function Expenses({expenses}) {
+function Expenses({items}) {
   return (
     <div className="expenses">
       <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
+        title={items[0].title}
+        amount={items[0].amount}
+        date={items[0].date}
       />
       <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
+        title={items[1].title}
+        amount={items[1].amount}
+        date={items[1].date}
       />
       <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
+        title={items[2].title}
+        amount={items[2].amount}
+        date={items[2].date}
       />
       <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
+        title={items[3].title}
+        amount={items[3].amount}
+        date={items[3].date}
       />
     </div>
   );
