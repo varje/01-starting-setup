b294ae8bc8eb970878fcc825dd4f142051020c04
varje <varjekivila@gmail.com>
2026-01-08 13:01:57 +0200

    refactor code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,7 +5,7 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
+  function clickHandler() {
     setTitle("New title");
   };
   return (
