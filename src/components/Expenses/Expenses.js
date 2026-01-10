ebcb0b27555b90fdee99059640e7e275a72cff32
varje <varjekivila@gmail.com>
2026-01-10 13:20:04 +0200

    add fallback text "no expenses found"

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,21 +14,27 @@ function Expenses({ items }) {
   const filteredExpenses = items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-  
+
+  let expensesContent = <p style={{color:"white"}}>No expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expensesContent}
     </Card>
   );
 }
